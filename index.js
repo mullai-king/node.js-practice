26565015db9a203f53ffad458c2fc9000945019f
mullai-king <52599344+mullai-king@users.noreply.github.com>
2024-04-01 14:54:53 +0530

    day 1 task

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import express from "express"
+import express from "express";
+import fs from "fs";
+import {format} from "date-fns"
+
 
 const app = express();
 const PORT = 4000;
@@ -14,5 +17,20 @@ app.get('/get-data',(req,res)=>{
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`);
 }) 
+
+app.get("/get-timestamp",(req,res)=>{
+ let today = format(new Date(),'dd-MM-yyyy-hh-mm-ss');
+ console.log(today);
+ const filePath = `Timestamp/${today}.txt`;
+ fs.writeFileSync(filePath, `${today}`, 'utf8');
+ let data =fs.readFileSync(filePath, 'utf8');
+ res.status(200).send({data});
+
+});
+
+app.get("/get-All-Files",(req,res)=>{
+  let files = fs.readdirSync("Timestamp");
+  res.status(200).send({files});
+})
 
 // app.use() //middleware
